d8ad50e7e54b68219f1c8c8766e95a9e22945e7f
Hijiahmed <hiji.ahmed.rae.js@tuwaiq.edu.sa>
2022-01-09 16:03:39 +0300

    .

--- routers/controllers/signUp.js
+++ routers/controllers/signUp.js
@@ -3,10 +3,10 @@ const userModel = require("../../db/models/userModels");
 const likeModel = require("../../db/models/likeModels");
 //
 const signUp = async (req, res) => {
-  let { name, email, password } = req.body;
+  let { name, email, password,admin } = req.body;
   try {
     password = await bcrypt.hash(password, 10);
-    const newSignUp = new userModel({ name, email, password });
+    const newSignUp = new userModel({ name, email, password ,admin});
     const response = await newSignUp.save();
     const newLike = new likeModel({user:response._id,like:[]});
     const result = await newLike.save();
