d99a4e4d907da5778a0ba403ba7278bc178a1f3b
Hijiahmed <hiji.ahmed.rae.js@tuwaiq.edu.sa>
2022-01-19 15:44:10 +0300

    newBackEnd

--- db/db.js
+++ db/db.js
@@ -1,6 +1,17 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");n
+// mongoose.connect("mongodb://localhost:27017/gamesDR").then(
+//   () => {
+//     console.log("DB connected");
+//   },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
 
-mongoose.connect("mongodb://localhost:27017/gamesDR").then(
+const mongoose = require("mongoose");
+require('dotenv').config()
+
+mongoose.connect(process.env.DB_URL).then(
   () => {
     console.log("DB connected");
   },
